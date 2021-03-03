c1b72412524b4e32b1e244c35116fd37733af841
Enrico Maria Montanari <e.montanarig4z@gmail.com>
2021-03-03 18:39:20 +0100

    Model for the player inventory

--- OOP20-talisman-csharp-task/characters/IInventoryModel.cs
+++ OOP20-talisman-csharp-task/characters/IInventoryModel.cs
@@ -1,7 +1,44 @@
-﻿namespace TalismanCSHARP
+﻿using System.Collections.Generic;
+using TalismanCSHARP.cards;
+
+namespace TalismanCSHARP.characters
 {
     public interface IInventoryModel
     {
-        void AddCard(Card card);
+        /// <summary>
+        /// Ass a card to the inventory
+        /// </summary>
+        /// <param name="card">instance of the card to add</param>
+        void AddCard(ref Card card);
+
+        /// <summary>
+        /// Remove a card from the inventory
+        /// </summary>
+        /// <param name="index">index of the card in the inventory</param>
+        void RemoveCard(ref int index);
+
+        /// <summary>
+        /// Remove a card from the inventory
+        /// </summary>
+        /// <param name="card">instance of the card to remove</param>
+        void RemoveCard(ref Card card);
+
+        /// <summary>
+        /// List all the cards in the inventory
+        /// </summary>
+        /// <returns>a list of all the cards in the inventory</returns>
+        ref List<Card> ListCards();
+
+        /// <summary>
+        /// Check if the player has the "Mule" card in his inventory
+        /// </summary>
+        /// <returns>true if the card is in the inventory</returns>
+        bool HasMule();
+
+        /// <summary>
+        /// Returns how many cards are in the player's inventory
+        /// </summary>
+        /// <returns>the amount of cards</returns>
+        int Size();
     }
 }
---
+++ OOP20-talisman-csharp-task/characters/InventoryModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using TalismanCSHARP.cards;
+using TalismanCSHARP.characters.exceptions;
+
+namespace TalismanCSHARP.characters
+{
+    public class InventoryModel : IInventoryModel
+    {
+        private List<Card> cards = new List<Card>();
+        
+        public void AddCard(ref Card card)
+        {
+            if (Size() < 4) cards.Add(card);
+            else throw new NotEnoughSpaceInventoryException();
+        }
+
+        public void RemoveCard(ref int index)
+        {
+            cards.RemoveAt(index);
+        }
+
+        public void RemoveCard(ref Card card)
+        {
+            cards.Remove(card);
+        }
+
+        public ref List<Card> ListCards()
+        {
+            return ref cards;
+        }
+
+        public bool HasMule()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Size()
+        {
+            return cards.Count;
+        }
+    }
+}
