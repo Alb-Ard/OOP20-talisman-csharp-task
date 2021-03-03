bcaa465d81b38273001b7378005cfe7f770ad41d
Enrico Maria Montanari <e.montanarig4z@gmail.com>
2021-03-03 18:45:46 +0100

    Exception for character's inventory

---
+++ OOP20-talisman-csharp-task/characters/exceptions/NotEnoughSpaceInventoryException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TalismanCSHARP.characters.exceptions
+{
+    public class NotEnoughSpaceInventoryException : Exception
+    {
+        public NotEnoughSpaceInventoryException() 
+        {
+        }
+        
+        public NotEnoughSpaceInventoryException(String message) : base(message)
+        {
+        }
+        
+        public NotEnoughSpaceInventoryException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
