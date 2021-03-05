0a400f0a45dc565eded70fc40aebe736c5af3914
Alberto Arduini <albertoa679@gmail.com>
2021-03-05 17:08:55 +0100

    Ricreato tabellone

---
+++ alberto.arduini/talisman-board-csharp/Model/Board/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talisman.Model.Board
+{
+    public class Board<S, C> : IBoard<S, C> where S : IBoardSection<C> where C : IBoardCell
+    {
+        private IList<S> sections;
+
+        public S this[int index] => sections[index];
+
+        public int SectionsCount => sections.Count;
+
+        public Board(IList<S> sections)
+        {
+            this.sections = new List<S>(sections);
+        }
+    }
+}
---
+++ alberto.arduini/talisman-board-csharp/Model/Board/IPopulatedBoard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talisman.Model.Board
+{
+    public interface IPopulatedBoard<S, C, P> : IBoard<S, C> where S: IBoardSection<C> where C: IBoardCell where P: IBoardPawn
+    {
+        int PawnsCount { get; }
+
+        P GetPawn(int index);
+        int GetPawnCell(int player);
+        int GetPawnSection(int player);
+        void MovePawnTo(int player, int cell);
+        void ChangePawnSection(int player, int section);
+        void ChangePawnSection(int player, int section, int cell);
+    }
+}
