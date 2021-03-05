e41ef91c3d52ea857fbce719e2ce0b3a9159a597
Alberto Arduini <albertoa679@gmail.com>
2021-03-05 17:05:58 +0100

    Ricreate celle

---
+++ alberto.arduini/talisman-board-csharp/Model/Board/IBoardCell.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talisman.Model.Board
+{
+    public enum CellType { Up, Down, Left, Right }
+
+    public interface IBoardCell
+    {
+        string Text { get; }
+        string ImagePath { get; }
+        CellType CellOrientationType { get; }
+    }
+}
---
+++ alberto.arduini/talisman-board-csharp/Model/Board/TalismanBoardCell.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Talisman.Model.Action;
+using Talisman.Model.Card;
+
+namespace Talisman.Model.Board
+{
+    public enum TalismanCellType { Biome, Zone, Enemy }
+
+    public class TalismanBoardCell : BoardCell
+    {
+        private readonly TalismanCellType talismanType;
+        private readonly IEnumerable<ITalismanAction> actions;
+
+        private Optional<ICard> card;
+
+        public TalismanCellType TalismanType => talismanType;
+        public IEnumerable<ITalismanAction> Actions => new List<ITalismanAction>(actions);
+
+        public ICard Card { get => card.Value; set => card = Optional<ICard>.Of(value); }
+
+        public TalismanBoardCell(string text, string imagePath, CellType cellType, TalismanCellType talismanType, IEnumerable<ITalismanAction> actions) : base(text, imagePath, cellType)
+        {
+            this.actions = new List<ITalismanAction>(actions);
+            this.talismanType = talismanType;
+        }
+
+        public void ClearCard()
+        {
+            card = Optional<ICard>.Empty();
+        }
+
+        public void ApplyActions()
+        {
+            Actions.ToList().ForEach(a => a.Apply());
+        }
+    }
+}
