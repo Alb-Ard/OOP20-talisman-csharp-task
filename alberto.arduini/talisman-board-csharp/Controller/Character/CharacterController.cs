205874d4b9c031790c80477e38aa9f312a3859b9
Alb-Ard <alberto.arduini3@studio.unibo.it>
2021-03-06 19:31:46 +0100

    Creata implementazione dummy per il character controller

---
+++ alberto.arduini/talisman-board-csharp/Controller/Character/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talisman.Controller.Character
+{
+    /// <summary>
+    /// Dummy implementation
+    /// </summary>
+    public class CharacterController : ICharactersController
+    {
+        public int CurrentPlayer => 0;
+    }
+}
